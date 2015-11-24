6b57cbd5436c484bb2d97607d84a3c85992f61eb
Paulus Schoutsen <paulus.schoutsen@mycase.com>
2015-11-24 15:02:42 -0800

    Update aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -2,3 +2,7 @@ alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
 alias kapow='touch ~/.pow/restart.txt'
+alias app='c mycase_app'
+alias be='bundle exec'
+alias bet='bundle exec ruby -Itest'
+
