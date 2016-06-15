30f640cb9cbd57b664a37310cda8db9d8ba1d0e6
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-06-14 18:15:18 -0700

    Update dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
 alias ha='cd /Users/paulus/dev/python/home-assistant'
-alias haweb='cd /Users/paulus/dev/python/home-assistant/homeassistant/components/frontend/www_static/home-assistant-polymer/'
+alias haweb='cd /Users/paulus/dev/python/home-assistant/homeassistant/components/frontend/www_static/home-assistant-polymer/ && nvm use'
+alias hajs='cd /Users/paulus/dev/python/home-assistant/homeassistant/components/frontend/www_static/home-assistant-polymer/home-assistant-js/ && nvm use'
+alias haio='cd /Users/paulus/dev/ruby/home-assistant.io'
+
