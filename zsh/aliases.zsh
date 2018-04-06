6d699985e664acddca107976d330ab0df1c5f1cb
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-04-06 10:09:34 -0400

    Update alias to polymer repo

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias reload!='. ~/.zshrc'
 alias ha='cd /Users/paulus/dev/python/home-assistant'
-alias haweb='cd /Users/paulus/dev/python/home-assistant/homeassistant/components/frontend/www_static/home-assistant-polymer/ && nvm use'
+alias haweb='cd /Users/paulus/dev/python/home-assistant-polymer/ && nvm use'
 alias hajs='cd /Users/paulus/dev/js/home-assistant-js-websocket'
 alias haio='cd /Users/paulus/dev/ruby/home-assistant.io'
 
