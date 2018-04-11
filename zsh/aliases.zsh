0224b05ec0db51d38478651346e9f6841b9a79a4
Paulus Schoutsen <balloob@gmail.com>
2018-04-11 15:48:03 -0400

    Update repo

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,5 +2,5 @@ alias reload!='. ~/.zshrc'
 alias ha='cd ~/dev/hass/home-assistant'
 alias haweb='cd ~/dev/hass/home-assistant-polymer/ && nvm use'
 alias hajs='cd ~/dev/hass/home-assistant-js-websocket'
-alias haio='cd ~/dev/hass/home-assistant.io'
+alias haio='cd ~/dev/hass/home-assistant.github.io'
 
